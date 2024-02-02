d04df873c6d923193dcd2174732dfe49bcd99df7
DanPlowman <47125582+DanPlowman@users.noreply.github.com>
2024-02-01 19:10:54 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
 function fib(n) {
-    return n;
+
+    var fib_ans =[0];
+    if (n==0)return fib_ans;
+
+    fib_ans[1]=1;
+    if (n==1)return fib_ans;
+
+    for(i=2; i<=n; i++) fib_ans[i] = fib_ans[i-1]+fib_ans[i-2];
+    
+    return fib_ans;
 }
